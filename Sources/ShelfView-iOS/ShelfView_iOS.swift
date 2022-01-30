d87b09a87e4810c500bded34a4d67d0ae1217f56
stephen payne <stephenpayne@MacBook-Pro.attlocal.net>
2022-01-30 08:22:42 -0500

    Adds package

---
+++ Sources/ShelfView-iOS/ShelfView_iOS.swift
@@ -0,0 +1,6 @@
+public struct ShelfView_iOS {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/ShelfView-iOSTests/ShelfView_iOSTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ShelfView_iOS
+
+final class ShelfView_iOSTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ShelfView_iOS().text, "Hello, World!")
+    }
+}
